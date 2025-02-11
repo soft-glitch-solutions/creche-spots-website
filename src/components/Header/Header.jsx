7c054542b1e7b5aa30e537efead5453f960b47ca
shaqeelless4 <shaqeelless4@gmail.com>
2025-02-11 14:13:10 +0000

    update changes.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
 import "./Header.css";
-import { BiMenuAltRight } from "react-icons/bi";
+import { BiMenuAltRight, BiLinkExternal } from "react-icons/bi"; // Added BiLinkExternal icon
 import { getMenuStyles } from "../../utils/common";
 import useHeaderColor from "../../hooks/useHeaderColor";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
-import ProfileMenu from "../ProfileMenu/ProfileMenu";
-import AddPropertyModal from "../AddPropertyModal/AddPropertyModal";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const headerColor = useHeaderColor();
-  const [modalOpened, setModalOpened] = useState(false);
 
-  const handleAddPropertyClick = () => {
-    setModalOpened(true);
-  };
-
-  // Function to navigate to the Creche Portal
   const handleLoginRedirect = () => {
     window.location.href = "https://online.crechespots.co.za/";
   };
@@ -25,30 +17,27 @@ const Header = () => {
   return (
     <section className="h-wrapper" style={{ background: headerColor }}>
       <div className="flexCenter innerWidth paddings h-container">
-        {/* logo */}
+        {/* Logo */}
         <Link to="/">
           <img src="./brand.png" alt="logo" width={200} />
         </Link>
 
-        {/* menu */}
-        <OutsideClickHandler
-          onOutsideClick={() => {
-            setMenuOpened(false);
-          }}
-        >
+        {/* Menu */}
+        <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
           <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-            <NavLink to="https://portal.crechespots.co.za/" target="_blank">
-              Creche Portal
+            <NavLink to="https://portal.crechespots.co.za/" target="_blank" rel="noopener noreferrer">
+              Creche Portal <BiLinkExternal size={16} />
             </NavLink>
-            <NavLink to="/price" >
+
+            <NavLink to="/price">
               Pricing
             </NavLink>
-            <NavLink to="https://online.crechespots.co.za/" target="_blank">
-              Parent App
+
+            <NavLink to="https://online.crechespots.co.za/" target="_blank" rel="noopener noreferrer">
+              Parent App <BiLinkExternal size={16} />
             </NavLink>
-            {/* add property */}
-            {/* login / sign up button */}
-            
+
+
             {/* Try Demo Button */}
             <a href="https://demo.crechespots.co.za/" target="_blank" rel="noopener noreferrer">
               <button className="button rainbow-button">Try Demo</button>
@@ -56,7 +45,7 @@ const Header = () => {
           </div>
         </OutsideClickHandler>
 
-        {/* for medium and small screens */}
+        {/* Mobile Menu Icon */}
         <div className="menu-icon" onClick={() => setMenuOpened((prev) => !prev)}>
           <BiMenuAltRight size={30} />
         </div>
